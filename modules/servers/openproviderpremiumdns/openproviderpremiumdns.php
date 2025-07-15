afc5fd2a96501f3e0d9814c978b99910782b49d4
avishka-ra <avishka.rathnavibhushana@openprovider.nl>
2025-07-15 14:47:55 +0930

    Correct structure and remove redundnt code

--- modules/servers/openproviderpremiumdns/openproviderpremiumdns.php
+++ modules/servers/openproviderpremiumdns/openproviderpremiumdns.php
@@ -4,7 +4,7 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'a
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'constants.php';
 
 use OpenproviderPremiumDns\controller\AccountController;
-use OpenproviderPremiumDns\helper\DNS;
+use OpenproviderPremiumDns\controller\DNSController;
 
 if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
@@ -105,46 +105,6 @@ function openproviderpremiumdns_ClientAreaCustomButtonArray()
  */
 function openproviderpremiumdns_manage_pdns(array $params)
 {
-    try {
-        if ($url = DNS::getDnsUrlOrFail($params)) {
-            $urlOne = $_SERVER['HTTP_REFERER'];
-            $url_decoded = html_entity_decode($urlOne);
-
-
-            // JavaScript confirm dialog
-            echo '<script type="text/javascript">
-                    document.addEventListener("DOMContentLoaded", function() {
-                        var userConfirmed = confirm("Do you want to open in New Tab?");
-                        if (userConfirmed) {
-                            var newWindow = window.open("' . $url . '", "_blank"); // Open OP DNS management page in a new tab
-                            if (newWindow) {
-                                window.location.href = "' . $url_decoded . '"; // Redirect to previous page
-                                newWindow.focus(); // Focus on the new tab
-                            } else {
-                                alert("New tab opening blocked! Please allow it for this site.");
-                                window.location.href = "' . $url . '"; // Redirect to OP DNS management page
-                            }
-                        } else {
-                            window.location.href = "' . $url . '"; // Redirect to OP DNS management page
-                        }
-                    });
-                    </script>';
-            exit;
-        } else {
-            throw new Exception("Failing to get DNS URL. Please check error logs for more details.");
-        }
-    } catch (Exception $e) {
-        // Record the error in WHMCS's module log.
-        \logModuleCall(
-            MODULE_NAME,
-            __FUNCTION__,
-            $params,
-            $e->getMessage(),
-            $e->getTraceAsString()
-        );
-
-        return $e->getMessage();
-    }
-
-    return SUCCESS_MESSAGE;
+    $controller = new DNSController();
+    return $controller->showManagePdns($params);
 }
